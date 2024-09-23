b1a234b31f8c2c709ee06f86625d7723ef43b499
Maca <maca.esc.k@gmail.com>
2024-09-23 16:12:06 -0300

    feat: pokemon details card content + styles: pokemon details card

--- src/components/details card/index.tsx
+++ src/components/details card/index.tsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 import styles from "./index.module.scss";
-import { StarOutlined, StarFilled } from '@ant-design/icons';
-import { EditOutlined } from '@ant-design/icons';
+import { StarOutlined, StarFilled, EditOutlined, ArrowRightOutlined } from '@ant-design/icons';
+import { Link } from "react-router-dom";
 
 interface IDetailsCard{
     id: number;
     name: string;
     image: string;
-    types: string[];
+    types: object[];
     height: number;
     weight: number;
     healthPoints: number;
     attackPoints: number;
     defensePoints: number;
-    speedPoints: number
+    speedPoints: number;
+    evolutions: string[];
 }
 
-export function DetailsCard({ id, name, image, types, height, weight, healthPoints, attackPoints, defensePoints, speedPoints }: IDetailsCard){
+export function DetailsCard({ id, name, image, types, height, weight, healthPoints, attackPoints, defensePoints, speedPoints, evolutions }: IDetailsCard){
     const [favorite, setFavorite] = useState(false);
 
     const star = favorite ? <StarFilled /> : <StarOutlined />
@@ -27,12 +28,79 @@ export function DetailsCard({ id, name, image, types, height, weight, healthPoin
 
     return(
         <div className={styles.detailsContainer}>
-            <div className={styles.pokemonImage}></div>
-            <div className={styles.pokemonDetails}>
-                <div className={styles.pokemonDetails_title}></div>
-                <div className={styles.pokemonDetails_details}></div>
+            <div className={styles.pokemonImage}>
+                <img src={image} alt="Imagen" />
             </div>
-            <div className={styles.pokemonEvolutions}></div>
+            <div className={styles.pokemonDetails}>
+                <div className={styles.pokemonDetails_header}>
+                    <div className={styles.pokemonDetails_title}>
+                        <h3>{id}</h3>
+                        <h3>{name}</h3>
+                    </div>
+                    <div className={styles.pokemonDetails_buttons}>
+                        <a className={favorite ? `${styles.pokemonDetails_buttons_favoriteBtn} ${styles.favorite}` : styles.pokemonDetails_buttons_favoriteBtn} onClick={handleClick}>{star}</a>
+                        <a className={styles.pokemonDetails_buttons_editBtn}><EditOutlined /></a>
+                    </div>
+                </div>
+                <div className={styles.pokemonDetails_details}>
+                    <div className={styles.pokemonDetails_details_traits}>
+                        <div>
+                            <span>Tipo:</span>
+                            <aside>
+                            {
+                                types.map(type => {
+                                    const {value, label} = type;
+
+                                    return(
+                                        <span style={{backgroundColor: value, padding: '0 1vw', borderRadius: '0.5vw', color: 'white'}}>{label}</span>
+                                    )
+                                })
+                            }
+                            </aside>
+                        </div>
+                        
+                        <div>
+                            <span>Altura:</span>
+                            <span>{height}</span>
+                        </div>
+                        <div>
+                            <span>Peso:</span>
+                            <span>{weight}</span>
+                        </div>
+                    </div>
+                    <div className={styles.pokemonDetails_details_stats}>
+                        <div>
+                            <span>Salud:</span>
+                            <span>{healthPoints}</span>
+                        </div>
+                        <div>
+                            <span>Ataque:</span>
+                            <span>{attackPoints}</span>
+                        </div>
+                        <div>
+                            <span>Defensa:</span>
+                            <span>{defensePoints}</span>
+                        </div>
+                        <div>
+                            <span>Velocidad:</span>
+                            <span>{speedPoints}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.pokemonEvolutions}>
+                {
+                    evolutions.map(evolution => {
+                        const flecha = evolutions.indexOf(evolution) == 0 ? '' : <ArrowRightOutlined />;
+                        return (
+                            <>
+                                {flecha}
+                                <Link to=''><img src={evolution} alt="Evolución" /></Link>
+                            </>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -1,10 +1,30 @@
 import { DetailsCard } from "../../components/details card";
 
 function Details(){
-    return 
+    return(
         <>
-            <DetailsCard />
+            <main className="mainCenter">
+                <DetailsCard
+                    id={1}
+                    name="Bulbasaur"
+                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"
+                    types={[
+                        { value: '#93d373', label: 'Planta' },
+                        { value: 'violet', label: 'Veneno' }]}
+                    height={1}
+                    weight={1}
+                    healthPoints={1}
+                    attackPoints={1}
+                    defensePoints={1}
+                    speedPoints={1}
+                    evolutions={[
+                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png',
+                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png',
+                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png']}
+                />
+            </main>
         </>
+    )
 }
 
 export default Details
