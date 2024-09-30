7b686793cd20ea75a8426eb2a50e69f7c5335db6
Maca <maca.esc.k@gmail.com>
2024-09-30 00:26:07 -0300

    feat: mock content

--- src/mock.ts
+++ src/mock.ts
@@ -1,76 +1,118 @@
+interface IEvolution{
+  name: string;
+  img: string;
+}
+
 export interface IPokemon{
-    
     id: number;
     name: string,
     types: string[];
+    height: number;
+    weigth: number;
     experience: number;
+    health: number
     attack: number;
     defence: number;
     speed: number;
-    height: number;
     img: string;
     source: string;
+    evolutions: IEvolution[];
 }
 
 export const listaPokemones:IPokemon[] = [
   {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["Grass", "Poison"],
+    height: 7,
+    weigth: 69,
+    experience: 64,
+    health: 45,
+    attack: 49,
+    defence: 49,
+    speed: 45,
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+    source: "https://pokeapi.co/api/v2/pokemon/1/",
+    evolutions: [
+      { name: "Bulbasaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" },
+      { name: "Ivysaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png" },
+      { name: "Venusaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png" }
+    ]
+  },
+  {
     id: 2,
     name: "Ivysaur",
     types: ["Grass", "Poison"],
+    height: 10,
+    weigth: 130,
     experience: 142,
+    health: 60,
     attack: 62,
     defence: 63,
     speed: 60,
-    height: 1.0,
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-    source: "https://pokeapi.co/api/v2/pokemon/2"
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png",
+    source: "https://pokeapi.co/api/v2/pokemon/2/",
+    evolutions: [
+      { name: "Bulbasaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" },
+      { name: "Ivysaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png" },
+      { name: "Venusaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png" }
+    ]
   },
   {
-    id: 6,
-    name: "Charizard",
-    types: ["Fire", "Flying"],
-    experience: 240,
-    attack: 84,
-    defence: 78,
-    speed: 100,
-    height: 1.7,
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-    source: "https://pokeapi.co/api/v2/pokemon/6"
+    id: 3,
+    name: "Venusaur",
+    types: ["Grass", "Poison"],
+    height: 20,
+    weigth: 1000,
+    experience: 236,
+    health: 80,
+    attack: 82,
+    defence: 83,
+    speed: 80,
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png",
+    source: "https://pokeapi.co/api/v2/pokemon/3/",
+    evolutions: [
+      { name: "Bulbasaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" },
+      { name: "Ivysaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png" },
+      { name: "Venusaur", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png" }
+    ]
   },
   {
-    id: 9,
-    name: "Blastoise",
-    types: ["Water"],
-    experience: 239,
-    attack: 83,
-    defence: 100,
-    speed: 78,
-    height: 1.6,
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
-    source: "https://pokeapi.co/api/v2/pokemon/9"
+    id: 4,
+    name: "Charmander",
+    types: ["Fire"],
+    height: 6,
+    weigth: 85,
+    experience: 62,
+    health: 39,
+    attack: 52,
+    defence: 43,
+    speed: 65,
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png",
+    source: "https://pokeapi.co/api/v2/pokemon/4/",
+    evolutions: [
+      { name: "Charmander", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png" },
+      { name: "Charmeleon", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png" },
+      { name: "Charizard", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png" }
+    ]
   },
   {
-    id: 25,
-    name: "Pikachu",
-    types: ["Electric"],
-    experience: 112,
-    attack: 55,
-    defence: 40,
-    speed: 90,
-    height: 0.4,
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-    source: "https://pokeapi.co/api/v2/pokemon/25"
-  },
-  {
-    id: 150,
-    name: "Mewtwo",
-    types: ["Psychic"],
-    experience: 306,
-    attack: 110,
-    defence: 90,
-    speed: 130,
-    height: 2.0,
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
-    source: "https://pokeapi.co/api/v2/pokemon/150"
+    id: 5,
+    name: "Charmeleon",
+    types: ["Fire"],
+    height: 11,
+    weigth: 190,
+    experience: 142,
+    health: 58,
+    attack: 64,
+    defence: 58,
+    speed: 80,
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png",
+    source: "https://pokeapi.co/api/v2/pokemon/5/",
+    evolutions: [
+      { name: "Charmander", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png" },
+      { name: "Charmeleon", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png" },
+      { name: "Charizard", img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png" }
+    ]
   }
 ];
