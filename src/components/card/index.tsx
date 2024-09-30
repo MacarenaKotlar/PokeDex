417e31d08ea01368f42a2f01adab68d0b61cdd53
Maca <maca.esc.k@gmail.com>
2024-09-30 14:24:20 -0300

    fix: card takes to detail page on click

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styles from "./index.module.scss";
 import { StarOutlined, StarFilled } from '@ant-design/icons';
 import { EditOutlined } from '@ant-design/icons';
+import { Link } from "react-router-dom";
 
 interface ICard{
     id: number;
@@ -20,24 +21,26 @@ export function Card({ id, name, image, attackPoints }: ICard){
     }
     
     return(
-        <div className={styles.pokemonCard}>
-            <div className={styles.pokemonCard_header}>
-                <div className={styles.pokemonCard_title}>
-                    <h3>{id}</h3>
-                    <h3>{name}</h3>
+        <Link to={`/detail/${id}`}>
+            <div className={styles.pokemonCard}>
+                <div className={styles.pokemonCard_header}>
+                    <div className={styles.pokemonCard_title}>
+                        <h3>{id}</h3>
+                        <h3>{name}</h3>
+                    </div>
+                    <div className={styles.pokemonCard_buttons}>
+                        <a className={favorite ? `${styles.pokemonCard_buttons_favoriteBtn} ${styles.favorite}` : styles.pokemonCard_buttons_favoriteBtn} onClick={handleClick}>{star}</a>
+                        <a className="pokemonCard-buttons-editBtn"><EditOutlined /></a>
+                    </div>
                 </div>
-                <div className={styles.pokemonCard_buttons}>
-                    <a className={favorite ? `${styles.pokemonCard_buttons_favoriteBtn} ${styles.favorite}` : styles.pokemonCard_buttons_favoriteBtn} onClick={handleClick}>{star}</a>
-                    <a className="pokemonCard-buttons-editBtn"><EditOutlined /></a>
+                <div className={styles.pokemonCard_main}>
+                    <img src={image} alt="Imagen"/>
+                    <div className={styles.pokemonCard_attack}>
+                        <span>Ataque:</span>
+                        <span>{attackPoints}</span>
+                    </div>
                 </div>
             </div>
-            <div className={styles.pokemonCard_main}>
-                <img src={image} alt="Imagen"/>
-                <div className={styles.pokemonCard_attack}>
-                    <span>Ataque:</span>
-                    <span>{attackPoints}</span>
-                </div>
-            </div>
-        </div>
+        </Link>
     )
 }
