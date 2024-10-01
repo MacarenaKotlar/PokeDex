8c14cc80e8238e64b1781faff5c2411e46f3c201
Maca <maca.esc.k@gmail.com>
2024-10-01 17:50:12 -0300

    feat: TagRender function

---
+++ src/components/tagRender/tagRender.tsx
@@ -0,0 +1,23 @@
+import { SelectProps, Tag } from "antd";
+import ColorTags from "../type tags/tags";
+
+type TagRender = SelectProps["tagRender"];
+
+export const TagRender: TagRender = (props) => {
+  const { label, value, closable, onClose } = props;
+  const onPreventMouseDown = (event: React.MouseEvent<HTMLSpanElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+  return (
+    <Tag
+      color={ColorTags(value)}
+      onMouseDown={onPreventMouseDown}
+      closable={closable}
+      onClose={onClose}
+      style={{ marginInlineEnd: 4 }}
+    >
+      {label}
+    </Tag>
+  );
+};
