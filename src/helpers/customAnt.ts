9bb8ac6336a6ee361e58ee49ad553965fa88fac2
Maca <maca.esc.k@gmail.com>
2024-10-01 17:44:35 -0300

    refactor: customAnt and selectOptionsTypes helpers

---
+++ src/helpers/customAnt.ts
@@ -0,0 +1,16 @@
+export const customAnt = {
+    "components": {
+      "Select": {
+        "colorBorder": "rgba(100,100,100,0.5)",
+        "fontSize": 15,
+        "colorPrimaryHover": "rgb(0,0,0,0.5)",
+        "colorPrimary": "rgb(0,0,0)"
+      },
+    "Input": {
+        "activeBorderColor": "rgb(0,0,0)",
+        "hoverBorderColor": "rgba(0,0,0,0.5)",
+        "colorBorder": "rgba(100,100,100,0.5)",
+        "inputFontSize": 15
+        }
+    }
+  }
---
+++ src/helpers/selectOptionsTypes.ts
@@ -0,0 +1,32 @@
+import { SelectProps } from "antd";
+
+export const typesOptions: SelectProps['options'] = [
+    {
+      label: 'Planta',
+      value: 'Grass',
+    },
+    {
+      label: 'Veneno',
+      value: 'Poison',
+    },
+    {
+      label: 'Fuego',
+      value: 'Fire',
+    },
+    {
+      label: 'Volador',
+      value: 'Flying',
+    },
+    {
+      label: 'Agua',
+      value: 'Water',
+    },
+    {
+      label: 'Eléctrico',
+      value: 'Electric',
+    },
+    {
+      label: 'Psíquico',
+      value: 'Psychic',
+    },
+  ];
