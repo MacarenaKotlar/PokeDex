568ca930ac9e01b7e307a59bc6b56ce3eec9f963
Maca <maca.esc.k@gmail.com>
2024-10-16 13:40:57 -0300

    feat: show pokemon details with API

--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -1,31 +1,37 @@
 import { useParams } from "react-router-dom";
 import { DetailsCard } from "../../components/details card";
 import { useEffect, useState } from "react";
-import { IPokemon, listaPokemones } from "../../mock";
+import { IPokemon } from "../../mock";
+import { GlobalStateService } from "../../services/globalStateService";
+import { PokemonUseCases } from "../../useCases/pokemonsUseCases";
 
 function Details() {
   const [pokemon, setPokemon] = useState<IPokemon>();
 
   const { id } = useParams();
 
-  useEffect(() => {
-    getPokemon();
-  }, []);
+  const pokemones = GlobalStateService.getPokemons();
 
-  const getPokemon = () => {
+  const getPokemon = async () => {
+    await PokemonUseCases.getPokemons();
     if (id) {
-      const fetch = listaPokemones.find(
+      const fetch = pokemones.find(
         (findPokemon) => parseInt(id) === findPokemon.id
       );
       setPokemon(fetch);
     }
   };
 
+  useEffect(() => {
+    getPokemon();
+  }, [id]);
+
   return (
     <>
+      {console.log(pokemon)}
       {pokemon && (
         <main className="mainCenter">
-          <DetailsCard pokemon={pokemon} />
+          <DetailsCard key={pokemon.id} pokemon={pokemon} />
         </main>
       )}
     </>
