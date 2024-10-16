91a999fde4ea499857cc4109a3b562f6936611f9
Maca <maca.esc.k@gmail.com>
2024-10-16 13:34:27 -0300

    fix: assign IPokemon interface to each pokemon in get method

--- src/services/api/apiService.ts
+++ src/services/api/apiService.ts
@@ -1,13 +1,45 @@
+import { IPokemon } from "../../mock"
 import { axiosInstance } from "./axiosInstance"
 
-const getPokemones = async () => {
+function firstLetterToUpperCase(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const getPokemonDetails = async (url: string): Promise<IPokemon> => {
+    const { data } = await axiosInstance.get(url);
+
+    return {
+        id: data.id,
+        name: firstLetterToUpperCase(data.name),
+        types: data.types.map((t: any) => t.type.name),
+        height: data.height,
+        weight: data.weight,
+        experience: data.base_experience,
+        health: data.stats.find((s: any) => s.stat.name === 'hp')?.base_stat || 0,
+        attack: data.stats.find((s: any) => s.stat.name === 'attack')?.base_stat || 0,
+        defense: data.stats.find((s: any) => s.stat.name === 'defense')?.base_stat || 0,
+        speed: data.stats.find((s: any) => s.stat.name === 'speed')?.base_stat || 0,
+        img: data.sprites.other['home'].front_default,
+        source: 'api',
+        evolutions: []
+    }
+}
+
+const getPokemones = async ({limit}:{limit?: number}) => {
     try{
-        const {data} = await axiosInstance.get('/pokemon')
-        return data.response
+        const {data} = await axiosInstance.get('/pokemon/', {params: {
+            limit
+        }});
+
+        const pokemons = await Promise.all(
+            data.results.map((pokemon: any) => getPokemonDetails(pokemon.url))
+        );
+        
+        return pokemons;
     }
 
     catch(e:any){
-        throw new Error(e.message)
+        throw new Error(e.message);
     }
 }
 
