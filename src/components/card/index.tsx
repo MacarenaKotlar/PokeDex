e11e79218f45301fd4b195138b0289934f951a52
Maca <maca.esc.k@gmail.com>
2024-10-28 15:28:54 -0300

    fix: card link path + edit button only on local pokemon cards

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -9,9 +9,10 @@ interface ICard {
   name: string;
   image: string;
   attackPoints: number;
+  source: string;
 }
 
-export function Card({ id, name, image, attackPoints }: ICard) {
+export function Card({ id, name, image, attackPoints, source }: ICard) {
   const location = useLocation();
   const [favorite, setFavorite] = useState(
     location.pathname.includes("favorites") ? true : false
@@ -28,12 +29,12 @@ export function Card({ id, name, image, attackPoints }: ICard) {
   };
 
   return (
-    <Link to={`/detail/${id}`}>
+    <Link to={`/detail/${source}/${id}`}>
       <div className={styles.pokemonCard}>
         <div className={styles.pokemonCard_header}>
           <div className={styles.pokemonCard_title}>
             <h3>{id}</h3>
-            <h3>{name}</h3>
+            <h3 className={styles.pokemon_name}>{name}</h3>
           </div>
           <div className={styles.pokemonCard_buttons}>
             <a
@@ -46,13 +47,15 @@ export function Card({ id, name, image, attackPoints }: ICard) {
             >
               {star}
             </a>
-            <Link to={`/edit/${id}`} className="pokemonCard-buttons-editBtn">
-              <EditOutlined />
-            </Link>
+            {source === "local" && (
+              <Link to={`/edit/${id}`} className="pokemonCard-buttons-editBtn">
+                <EditOutlined />
+              </Link>
+            )}
           </div>
         </div>
         <div className={styles.pokemonCard_main}>
-          <img src={image} alt="Imagen" />
+          <img src={image} alt={name} />
           <div className={styles.pokemonCard_attack}>
             <span>Ataque:</span>
             <span>{attackPoints}</span>
