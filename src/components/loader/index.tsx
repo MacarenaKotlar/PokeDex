92837f56ad8c198b6d3b4dd2320616fbe0c25ed8
Maca <maca.esc.k@gmail.com>
2024-11-06 14:36:06 -0300

    style: loader

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -3,8 +3,7 @@ import styles from "./index.module.scss";
 export function Loader() {
   return (
     <div className={styles.loaderContainer}>
-      <span>Cargando</span>
-      <img src="/PokeBall.png" alt="PokeBall" />
+      <div className={styles.loader} />
     </div>
   );
 }
