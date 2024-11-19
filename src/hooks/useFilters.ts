bb5aede3f628ab0f1f7dbf9219846f2c70984802
Maca <maca.esc.k@gmail.com>
2024-11-19 14:19:24 -0300

    refactor: add error verification at useFilters

--- src/hooks/useFilters.ts
+++ src/hooks/useFilters.ts
@@ -3,5 +3,9 @@ import { FiltersContext } from "../context/filters";
 
 export function useFilters() {
     const { filters, setFilters } = useContext(FiltersContext);
+
+    if(filters === undefined) throw new Error("filters must be used within a FiltersProvider");
+    if(setFilters === undefined) throw new Error("setFilters must be used within a FiltersProvider");
+    
     return { filters, setFilters };
   }
