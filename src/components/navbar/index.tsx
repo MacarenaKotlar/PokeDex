8a35c906b8877f9b0783f9006be913162785091f
Maca <maca.esc.k@gmail.com>
2024-11-23 01:53:48 -0300

    style: change navbar element text and underline to blue when the route path matches the element

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,18 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./index.module.scss";
 
 export function NavBar() {
+  const location = useLocation();
+
   return (
     <header>
       <nav className={styles.header_navbar}>
         <ul>
-          <li>
+          <li className={location.pathname === "/" ? styles.active : ""}>
             <Link to="/">Inicio</Link>
           </li>
-          <li>
+          <li
+            className={
+              location.pathname.includes("favorites") ? styles.active : ""
+            }
+          >
             <Link to="/favorites">Favoritos</Link>
           </li>
-          <li>
+          <li
+            className={
+              location.pathname.includes("creation") ? styles.active : ""
+            }
+          >
             <Link to="/creation">Crear Pokémon</Link>
           </li>
         </ul>
